da3e4caeaf395874bb4f1f23eac6f83654c36218
Dan Kubb <dan.kubb@gmail.com>
2012-03-22 23:39:48 -0700

    Change comment to be more straight forward

--- lib/ice_nine/freezer.rb
+++ lib/ice_nine/freezer.rb
@@ -5,8 +5,7 @@ module IceNine
   # The default class that handles freezing objects
   class Freezer
 
-    # Under 1.9 configure const_get and const_defined?
-    # to not search ancestors for constants
+    # Configure const_get and const_defined? to not search ancestors
     SKIP_ANCESTORS = (RUBY_VERSION < '1.9' ? [] : [ false ]).freeze
 
     # Lookup the Freezer subclass by object type
