919ac8ddb4404b3aa86141f0bde0a98db0dc96e6
Dan Kubb <dan.kubb@gmail.com>
2012-03-26 23:38:35 -0700

    Move optional Object extension into explicit module
    * I prefer to monkey patch core classes using this approach since it makes it
      explicit where things are coming from given that this module will become part
      of the object ancestors.

--- lib/ice_nine/core_ext/object.rb
+++ lib/ice_nine/core_ext/object.rb
@@ -1,18 +1,28 @@
 # encoding: utf-8
 
-# Base class for every object in Ruby
-class Object
+module IceNine
 
-  # Deep freeze an object
-  #
-  # @example
-  #   object = object.deep_freeze
-  #
-  # @return [self]
-  #
-  # @api public
-  def deep_freeze
-    IceNine.deep_freeze(self)
-  end
+  # Core Ruby extensions
+  module CoreExt
 
-end # class Object
+    # Extend Object with deep freezing
+    module Object
+
+      # Deep freeze an object
+      #
+      # @example
+      #   object = object.deep_freeze
+      #
+      # @return [self]
+      #
+      # @api public
+      def deep_freeze
+        IceNine.deep_freeze(self)
+      end
+
+    end #module Object
+  end # module CoreExt
+end # module IceNine
+
+# Add Object#deep_freeze
+Object.instance_eval { include IceNine::CoreExt::Object }
