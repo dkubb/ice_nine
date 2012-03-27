d601c75a99003c5754ea9f27e863d6e06def741c
Dan Kubb <dan.kubb@gmail.com>
2012-03-27 00:54:22 -0700

    Move require statement below other libs that create dependent constants

--- lib/ice_nine.rb
+++ lib/ice_nine.rb
@@ -5,13 +5,14 @@ require 'set'
 require 'ice_nine/support/recursion_guard'
 
 require 'ice_nine/freezer'
-require 'ice_nine/freezer/no_freeze'
 require 'ice_nine/freezer/object'
 
 require 'ice_nine/freezer/array'
 require 'ice_nine/freezer/hash'
 require 'ice_nine/freezer/range'
 require 'ice_nine/freezer/struct'
+
+require 'ice_nine/freezer/no_freeze'
 
 require 'ice_nine/version'
 
