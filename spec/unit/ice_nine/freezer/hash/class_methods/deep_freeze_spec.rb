bada0885df7fc6e0ac08e1db78bf23fd6867cfe3
Dan Kubb <dan.kubb@gmail.com>
2013-06-16 23:56:53 -0700

    Update Hash freezer to freeze the default

--- spec/unit/ice_nine/freezer/hash/class_methods/deep_freeze_spec.rb
+++ spec/unit/ice_nine/freezer/hash/class_methods/deep_freeze_spec.rb
@@ -8,8 +8,10 @@ describe IceNine::Freezer::Hash, '.deep_freeze' do
 
   let(:object) { described_class }
 
-  context 'with a Hash object' do
-    let(:value) { { Object.new => Object.new } }
+  context 'with a Hash object having a default proc' do
+    let(:value) do
+      Hash.new {}.update(Object.new => Object.new)
+    end
 
     it 'returns the object' do
       should be(value)
@@ -17,6 +19,46 @@ describe IceNine::Freezer::Hash, '.deep_freeze' do
 
     it 'freezes the object' do
       expect { subject }.to change(value, :frozen?).from(false).to(true)
+    end
+
+    it 'freezes the default proc' do
+      subject.default_proc.should be_frozen
+    end
+
+    it 'freezes each key in the Hash' do
+      subject.keys.select(&:frozen?).should == subject.keys
+    end
+
+    it 'freezes each value in the Hash' do
+      subject.values.select(&:frozen?).should == subject.values
+    end
+
+    if RUBY_VERSION >= '1.9' and RUBY_ENGINE == 'rbx'
+      it 'does not freeze the Hash state' do
+        subject.instance_variable_get(:@state).should_not be_frozen
+      end
+
+      it 'does not freeze the Hash entries' do
+        subject.instance_variable_get(:@entries).should_not be_frozen
+      end
+    end
+  end
+
+  context 'with a Hash object having a default value' do
+    let(:value) do
+      Hash.new('').update(Object.new => Object.new)
+    end
+
+    it 'returns the object' do
+      should be(value)
+    end
+
+    it 'freezes the object' do
+      expect { subject }.to change(value, :frozen?).from(false).to(true)
+    end
+
+    it 'freezes the default value' do
+      subject.default.should be_frozen
     end
 
     it 'freezes each key in the Hash' do
