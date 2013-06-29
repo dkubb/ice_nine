b3059beacd9291f4edb1aa7d8b8ecfbd58c23bb3
Dan Kubb <dan.kubb@gmail.com>
2013-06-29 16:12:07 -0700

    Add simplecov and coveralls to ice_nine

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,30 @@
 # encoding: utf-8
-require 'ice_nine'
+
 require 'devtools/spec_helper'
+
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+
+  SimpleCov.start do
+    command_name     'spec:unit'
+    add_filter       'config'
+    add_filter       'spec'
+    minimum_coverage 100
+  end
+end
+
+require 'ice_nine'
+
+# require spec support files and shared behavior
+Dir[File.expand_path('../{support,shared}/**/*.rb', __FILE__)].each do |file|
+  require file
+end
 
 RSpec.configure do |config|
 end
