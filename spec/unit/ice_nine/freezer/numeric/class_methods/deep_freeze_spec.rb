ebeabe0ad612b3e3e94b8f7a96e05c5fe76ef1de
Dan Kubb <dan.kubb@gmail.com>
2013-09-19 00:48:18 -0700

    Add bigdecimal specs for numeric freezer

--- spec/unit/ice_nine/freezer/numeric/class_methods/deep_freeze_spec.rb
+++ spec/unit/ice_nine/freezer/numeric/class_methods/deep_freeze_spec.rb
@@ -4,13 +4,14 @@ require 'spec_helper'
 require 'ice_nine/freezer'
 require 'ice_nine/freezer/no_freeze'
 require 'ice_nine/freezer/numeric'
+require 'bigdecimal'
 
 describe IceNine::Freezer::Numeric, '.deep_freeze' do
   subject { object.deep_freeze(value) }
 
   let(:object) { described_class }
 
-  [0.0, 0, 0x7fffffffffffffff].each do |value|
+  [0.0, 0, 0x7fffffffffffffff, BigDecimal('0')].each do |value|
     context "with a #{value.class} object" do
       let(:value) { value }
 
