a44ff80c408a4c216e311abb2c8d4eb3672d0746
Dan Kubb <dan.kubb@gmail.com>
2013-09-18 23:25:17 -0700

    Add spec for circular references in IceNine::Freezer::Range.deep_freeze

--- spec/unit/ice_nine/freezer/range/class_methods/deep_freeze_spec.rb
+++ spec/unit/ice_nine/freezer/range/class_methods/deep_freeze_spec.rb
@@ -8,9 +8,32 @@ describe IceNine::Freezer::Range, '.deep_freeze' do
 
   let(:object) { described_class }
 
-  context 'with a Range' do
-    let(:value) { 'a'..'z' }
+  let(:element_class) do
+    Class.new do
+      attr_reader :number, :range
+      protected :number, :range
 
+      def initialize(number, range = nil)
+        @number = number
+        @range  = nil
+      end
+
+      def succ
+        self.class.new(number.succ, range)
+      end
+
+      def <=>(other)
+        range <=> other.range && number <=> other.number
+      end
+
+      # allow for circular references
+      def range=(range)
+        @range = range
+      end
+    end
+  end
+
+  shared_examples 'IceNine::Freezer::Range.deep_freeze' do
     it 'returns the object' do
       should be(value)
     end
@@ -25,6 +48,26 @@ describe IceNine::Freezer::Range, '.deep_freeze' do
 
     it 'freeze the last object in the Range' do
       expect(subject.end).to be_frozen
+    end
+  end
+
+  context 'with a Range' do
+    let(:value) { element_class.new(1)..element_class.new(100) }
+
+    context 'without a circular reference' do
+      it_behaves_like 'IceNine::Freezer::Range.deep_freeze'
+    end
+
+    context 'with a circular reference in the first element' do
+      before { value.begin.range = value }
+
+      it_behaves_like 'IceNine::Freezer::Range.deep_freeze'
+    end
+
+    context 'with a circular reference in the last element' do
+      before { value.end.range = value }
+
+      it_behaves_like 'IceNine::Freezer::Range.deep_freeze'
     end
   end
 end
