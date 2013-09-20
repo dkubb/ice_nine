828f37d51dfbb86f05570736b91bae51e3375e5a
Trent Ogren <tedwardo2@gmail.com>
2013-09-20 00:12:49 -0500

    Add integration tests for fast_deep_freeze

--- spec/integration/ice_nine/class_methods/fast_deep_freeze_spec.rb
+++ spec/integration/ice_nine/class_methods/fast_deep_freeze_spec.rb
@@ -4,16 +4,16 @@ require 'spec_helper'
 require 'ice_nine'
 require 'delegate'
 
-describe IceNine, '.deep_freeze' do
-  subject { object.deep_freeze(value) }
+describe IceNine, '.fast_deep_freeze' do
+  subject { object.fast_deep_freeze(value) }
 
   let(:object) { IceNine }
 
   context 'with a shallowly frozen value' do
-    let(:value) { ['a', ['b', 'c']].freeze }
+    let(:value) { ['a', 'b'].freeze }
 
     it 'does not deep freeze' do
-      expect(subject.select(&:frozen?)).to eql(value)
+      expect(subject.select(&:frozen?)).to eql([])
     end
   end
 
